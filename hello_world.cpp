1c4e2f52df15ba5c4c16cb8c2da17d3bf66dd8d4
Daisuke0713 <yamadad@carleton.edu>
2021-11-21 13:05:48 +0100

    init

---
+++ hello_world.cpp
@@ -0,0 +1,17 @@
+/**
+ * Reviewing some C++ basic code
+**/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, world!" << endl;
+
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "You entered " << number << endl;
+    return 0;
+}
